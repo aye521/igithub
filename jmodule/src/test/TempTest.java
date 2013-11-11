8403e0ab34d34939d8844ca2e1ec6bfe663cae97
aye521 <echo.368@163.com>
2013-11-11 15:42:43 +0800

    ignore

--- jmodule/src/test/TempTest.java
+++ jmodule/src/test/TempTest.java
@@ -4,5 +4,17 @@ public class TempTest {
 
 	public static void main(String[] args) {
 		System.out.println("first git java file test");
+		int condition = 3;
+		switch (condition) {
+		case 1:
+			System.out.println(1);
+			break;
+		case 2:
+			System.out.println(2);
+			break;
+		default:
+			System.out.println(3);
+			break;
+		}
 	}
 }
