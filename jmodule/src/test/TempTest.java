5a211b6f1ee689b80f83f5f7508ab35126aa6f09
aye521 <echo.368@163.com>
2014-03-26 15:48:19 +0800

    add utf16 test

--- jmodule/src/test/TempTest.java
+++ jmodule/src/test/TempTest.java
@@ -1,21 +1,12 @@
 package test;
 
+import java.net.URL;
+
 public class TempTest {
 
 	public static void main(String[] args) {
-		System.out.println("first git java file ");
 		
-		int condition = 3;
-		switch (condition) {
-		case 1:
-			System.out.println(1);
-			break;
-		case 2:
-			System.out.println(2);
-			break;
-		default:
-			System.out.println(3);
-			break;
-		}
+		char[] c = "1234567890".toCharArray();
+	
 	}
 }
